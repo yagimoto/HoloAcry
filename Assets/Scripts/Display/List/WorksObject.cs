495d76eec0377a4da5317bd60b5301130e7e391e
sungyo4869 <hchengjian1@gmail.com>
2023-09-21 23:59:17 +0900

    [update] 生成されるごとにWorkSpaceの名前が変わるようになった

--- Assets/Scripts/Display/List/WorksObject.cs
+++ Assets/Scripts/Display/List/WorksObject.cs
@@ -5,6 +5,9 @@ public class WorksObject : MonoBehaviour
     public GameObject WorkSpace;
     public void CreateWork()
     {
-        GameManager.CurrentWork = Instantiate(WorkSpace, this.transform);
+        GameObject NewWork = Instantiate(WorkSpace, this.transform);
+        NewWork.transform.name = "Work" + this.transform.childCount.ToString();
+
+        GameManager.CurrentWork = NewWork;
     }
 }
