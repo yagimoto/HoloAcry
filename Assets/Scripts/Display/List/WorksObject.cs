d1cda24d71edeb3c6a95618e0127e8120224191c
sungyo4869 <hchengjian1@gmail.com>
2023-09-22 17:54:06 +0900

    [update] 生成されるごとにWorkSpaceの生成位置が変わるようにした

--- Assets/Scripts/Display/List/WorksObject.cs
+++ Assets/Scripts/Display/List/WorksObject.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class WorksObject : MonoBehaviour
 {
     public GameObject WorkSpace;
+    private float x = 0;
+    private float y = 125;
     public void CreateWork()
     {
         GameObject NewWork = Instantiate(WorkSpace, this.transform);
+
+        x = ( this.transform.childCount % 2 ) == 0 ? 125.0f : 0.0f;
+        y = ( this.transform.childCount % 2 ) == 0 ? y : y - 125.0f;
+        
+        NewWork.transform.localPosition = new Vector3(x , y, 0.0f);
         NewWork.transform.name = "Work" + this.transform.childCount.ToString();
 
         GameManager.CurrentWork = NewWork;
